93936446769c1fc53dd6c9a9714104f02f0c3716
Pascal <pascal.reinhardt@gmx.net>
2023-09-16 19:02:49 +0200

    Erste Version der Methode "zugMachen" geschrieben.

---
+++ src/spieler/MenschSpieler.java
@@ -0,0 +1,36 @@
+
+package spieler;
+
+import spielfeld.Spielfeld;
+
+import java.util.Scanner;
+import spielfeld.Spielfeld;
+import validieren.Validiere;
+
+public class MenschSpieler extends Spieler{
+
+
+    public MenschSpieler(String name, char symbol){
+        super(name, symbol);
+    }
+
+    @Override
+    public int zugMachen() {
+        Validiere val = new Validiere();
+        Scanner sc = new Scanner(System.in);
+        try{
+        while(true){
+        System.out.println("Geben Sie die Zahl des Feldes ein, in das Sie setzen wollen!");
+        int feld = sc.nextInt();
+            if (val.validiereIndex(feld)) return feld;
+            }
+        }catch (Exception e){
+            System.out.println("Es wurde ein Fehler beim Validieren Ihres angegebenen Wertes festgestellt" + e.getMessage());
+        }finally {
+            sc.close();
+        }return 0;
+    }
+
+
+
+}
