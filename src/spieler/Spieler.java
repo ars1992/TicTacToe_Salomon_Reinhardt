d5402fb62db573550d943f08c7aa2517ccc903e1
Pascal <pascal.reinhardt@gmx.net>
2023-09-16 16:20:36 +0200

    Einrichtung + Anlegen des Packages, Erstellen der abstrakten Klasse, Konstruktor hinzugefügt

---
+++ src/spieler/Spieler.java
@@ -0,0 +1,14 @@
+package spieler;
+
+public abstract class Spieler {
+
+    private String name;
+    private char symbol;
+
+    public Spieler(String name, char symbol){
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+
+}
