b3049eb74241996fba84b74dcf140282f48bff22
Pascal <pascal.reinhardt@gmx.net>
2023-09-18 11:01:55 +0200

    Klasse ZufallsSpieler erstellt, Methode zugMachen geschrieben.

---
+++ src/spieler/ZufallsSpieler.java
@@ -0,0 +1,26 @@
+package spieler;
+import spielfeld.Spielfeld;
+import validieren.Validiere;
+import java.util.Random;
+
+
+
+public class ZufallsSpieler extends Spieler{
+
+    public ZufallsSpieler(String name, char symbol){
+        super(name,symbol);
+    }
+
+    @Override
+    public int zugMachen() {
+        Validiere val = new Validiere();
+        Random rand = new Random();
+        int zufallszahl = rand.nextInt(9)+1;
+        try {
+            if (val.validiereIndex(zufallszahl)) return zufallszahl;
+        }catch(Exception e){
+            System.out.println("Es wurde ein Fehler beim Zug von ZufallsSpieler " + e.getMessage());
+        }
+        return -1;
+    }
+}
