524d11a1c602466266691d1ed3052d8d523d299f
ars1992 <alessandro.salomon@gmx.de>
2023-09-23 22:28:35 +0200

    test validiere index

--- test/validiererTest/ValidiererTest.java
+++ test/validiererTest/ValidiererTest.java
@@ -7,11 +7,18 @@ import org.junit.Test;
 
 public class ValidiererTest {
 
+    @Test
     public void testValiedireIndex(){
+        System.out.println("TEST validiereIndex");
         Validiere validiere = new Validiere();
         for (int i = 1; i <= 9; i++){
             Assert.assertTrue(validiere.validiereIndex(i));
         }
+        Assert.assertFalse(validiere.validiereIndex(10));
+        Assert.assertFalse(validiere.validiereIndex(0));
     }
+
+    @Test
+    public void test
 
 }
