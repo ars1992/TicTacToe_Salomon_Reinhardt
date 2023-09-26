4f39de2dc4f778f17720b240ff48ca14a26aeeaf
Pascal <pascal.reinhardt@gmx.net>
2023-09-26 16:01:36 +0200

    Testmethode für MenschSpieler geschrieben und dann festgestellt, dass die Ausführung gar nicht möglich ist (wg. Scanner)

--- test/spielerTests/MenschSpielerTest.java
+++ test/spielerTests/MenschSpielerTest.java
@@ -1,4 +1,34 @@
 package spielerTests;
 
+import org.junit.*;
+import spieler.MenschSpieler;
+import java.util.Scanner;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class MenschSpielerTest {
+
+    @Test
+    public void testMacheZug(){
+        System.out.println("Testversuch des Menschspielers,der einen Zug zu machen will:");
+        MenschSpieler mensch = new MenschSpieler("Yury", 'X');
+        String input = "10";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        int zug = mensch.zugMachen();
+
+        assertEquals(10, zug);
+
+        String consoleOutput = outputStream.toString().trim();
+        assertFalse(consoleOutput.contains("Ungültige eingabe!"));
+
+    }
+
+
 }
