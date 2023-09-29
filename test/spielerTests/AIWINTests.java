fea711c8725f6a289a039538b44bab6340fc8580
Pascal <pascal.reinhardt@gmx.net>
2023-09-29 14:27:38 +0200

    Unterklassen ergänzt.

--- test/spielerTests/AIWINTests.java
+++ test/spielerTests/AIWINTests.java
@@ -1,16 +1,22 @@
 package spielerTests;
 import spieler.GewinnerSpieler;
 import org.junit.Test;
+import spieler.MenschSpieler;
+import spieler.Spieler;
+import spieler.ZufallsSpieler;
+import spielfeld.Spielfeld;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class AIWINTests {
 
+/*
     //Erkenntniss: Testmethoden für die Spieler sind in diesem Fall, in keinem Fall möglich.
 
     @Test
@@ -31,4 +37,71 @@ public class AIWINTests {
         assertFalse(consoleOutput.contains("Ungültige eingabe!"));
 
     }
+
+String[][] namen = {
+                {"TicTacToe-Terminator", "X-ecuter", "Xena, die X-Maschine", "BlockBuster", "TickyTacky-Troll"},
+                {"Nullox der Nullenmeister", "KreisKönig", "O-Orakel", "O-Hexe", "Glücklicher Zufall"}
+        }
+
+ */
+
+    String[] namen = {
+            "TicTacToe-Terminator", "X-ecuter", "Xena, die X-Maschine", "BlockBuster", "TickyTacky-Troll",
+            "Nullox der Nullenmeister", "KreisKönig", "O-Orakel", "O-Hexe", "Glücklicher Zufall"};
+
+
+   // String name = namen[0][(int) (Math.random() * namen[0].length)];
+   // String name2 = namen[1][(int) (Math.random() * namen[1].length)];
+@Test
+public void testKonstruktor(){
+    System.out.println("Objekt-Test:");
+    Spieler testObjekt = new GewinnerSpieler('X'); //....getSymbol());
+    assertNotNull(testObjekt);
+    assertTrue(testObjekt instanceof GewinnerSpieler);
+    String name = testObjekt.getName();
+    assertTrue(Arrays.asList(namen).contains(name));
+
+}
+
+    @Test
+    public void testGetName(){                                       //Nur für
+        Spieler testObjekt = new GewinnerSpieler( 'X');
+        System.out.println("Test der getName Methode:");
+        assertTrue(testObjekt.getName(),Arrays.asList(namen).contains(testObjekt.getName()) );
+        Object resultat = testObjekt.getName();
+        System.out.println("Test ob der Rückgabewert ungleich NULL und ein String ist:");
+        assertTrue((resultat instanceof String) && resultat != null);
+
+    }
+
+    /*
+    public void setName(){
+        System.out.println("Test der setName-Methode:");    //überflüssig bzw. nicht möglich.
+        Spieler testObjekt = new GewinnerSpieler( 'X');
+        testObjekt.setName("Hans");
+        assertEquals("Hans", testObjekt.getName());
+    }*/
+
+
+    @Test
+    public void testGetSymbol(){
+        Spieler testObjekt = new GewinnerSpieler('X');
+        System.out.println("Test der getSymbol-Methode:");
+        assertEquals('X', testObjekt.getSymbol());
+        Object symbol = testObjekt.getSymbol();
+        assertTrue(symbol instanceof Character);
+
+    }
+
+    @Test
+    public void testGetSpielfeld(){
+        System.out.println("Test der getSpielfeld_Methode:");
+        Spieler spieler = new GewinnerSpieler('X');
+        Object spielfeld  = spieler.getSpielfeld();
+        assertTrue(spielfeld instanceof Spielfeld);
+        assertNotNull(spielfeld);
+
+    }
+
+
 }
--- test/spielerTests/zufallsSpielerTest.java
+++ test/spielerTests/zufallsSpielerTest.java
@@ -1,19 +1,31 @@
 package spielerTests;
 import org.junit.Assert.*;
 import org.junit.Test;
+import spieler.MenschSpieler;
 import spieler.ZufallsSpieler;
 
 import static org.junit.Assert.*;
 
-public class zufallsSpielerTest {
+public class zufallsSpielerTest extends SpielerTests{
 
-    @Test
+    @Test@Override
     public void testKonstruktor(){
         System.out.println("Objekt-Test:");
         ZufallsSpieler zufallTestObjekt = new ZufallsSpieler('X');
-        assertEquals('X', zufallTestObjekt.getName());
+        assertEquals(zufallTestObjekt.getSymbol(),'X' );
         assertNotNull(zufallTestObjekt);
+        assertNotNull(zufallTestObjekt.getName());
         assertTrue(zufallTestObjekt instanceof ZufallsSpieler);
+    }
+
+
+    @Override@Test
+    public void testGetName(){
+        ZufallsSpieler spieler = new ZufallsSpieler('X');
+        System.out.println("Test der getName-Methode");
+        assertNotNull(spieler.getName());
+        String name = spieler.getName();
+        assertTrue(name instanceof String);
     }
 
 
