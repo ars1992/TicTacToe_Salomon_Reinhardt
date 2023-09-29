8be744e70ed0e898ca86dc1d09431a5709b48378
Pascal <pascal.reinhardt@gmx.net>
2023-09-29 13:25:03 +0200

    Abstrakte Test-Klasse für Spieler-Methoden geschrieben.

--- test/spielerTests/zufallsSpielerTest.java
+++ test/spielerTests/zufallsSpielerTest.java
@@ -1,6 +1,22 @@
 package spielerTests;
+import org.junit.Assert.*;
+import org.junit.Test;
+import spieler.ZufallsSpieler;
+
+import static org.junit.Assert.*;
 
 public class zufallsSpielerTest {
 
-    //Testmethode sinnlos, da random.
+    @Test
+    public void testKonstruktor(){
+        System.out.println("Objekt-Test:");
+        ZufallsSpieler zufallTestObjekt = new ZufallsSpieler('X');
+        assertEquals('X', zufallTestObjekt.getName());
+        assertNotNull(zufallTestObjekt);
+        assertTrue(zufallTestObjekt instanceof ZufallsSpieler);
+    }
+
+
+
+
 }
